7763a87c147856d5efeae4c37d316d51819a4ea3
israel kilday <israel.kilday@yahoo.com.br>
2022-03-24 01:07:26 -0300

    mudanças em type-string.js

---
+++ type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "Ola, mundo!";
+const texto2 = 'Ola, mundo!';
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "3457";
+
+const citacao = "meu nome é ";
+const meuNome = "Leonardo";
+
+//concatenação (+)
+
+console.log(citacao + meuNome);
+
+//template string ou template literal
