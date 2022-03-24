922e40548c64dfe2342b1bffc68eaec6b2978e7e
israel kilday <israel.kilday@yahoo.com.br>
2022-03-24 05:47:58 -0300

    codigos var e conversoes

---
+++ converçoes.js
@@ -0,0 +1,13 @@
+//tipo de dado
+//booleano
+
+//convenção implicita
+ const numero = 456;
+ const numeroString = Number("456");
+//Number
+//string
+
+ console.log(numero + numeroString);
+
+ //convençao explicita
+
