361a07bdb7bc9c668d4265c3c1d621e61d8d2358
israel kilday <israel.kilday@yahoo.com.br>
2022-03-24 02:24:47 -0300

    mais exemplos

--- type-boolean2.js
+++
@@ -1,9 +0,0 @@
-// boolean
-// true -> verdadeiro
-// false -> falso
-
-const primeiroNumero = 5;
-const segundoNumero = 5;
-let cadastroAtivo = true;
-
-console.log(primeiroNumero === segundoNumero);
